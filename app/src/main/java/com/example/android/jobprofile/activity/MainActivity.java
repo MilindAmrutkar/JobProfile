9b9e51626644387960365d7052d4a13e35ae8b09
MilindAmrutkar <milind27.amrutkar@gmail.com>
2019-02-27 15:59:40 +0530

    Added Glide to fetch image from the url

--- app/src/main/java/com/example/android/jobprofile/activity/MainActivity.java
+++ app/src/main/java/com/example/android/jobprofile/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.jobprofile.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,14 +15,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.android.jobprofile.R;
 import com.example.android.jobprofile.model.Employee;
 import com.example.android.jobprofile.model.Skill;
 import com.example.android.jobprofile.rest.ApiClient;
 import com.example.android.jobprofile.rest.ApiInterface;
 
+import java.io.DataOutputStream;
 import java.util.List;
+import java.util.Locale;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,24 +43,66 @@ public class MainActivity extends AppCompatActivity {
 
     private final static String authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjdiYmZlNTI4YjI5OWVhOGZjNTg4OWM3N2M5NWZjMDliOGVmNmI0NGNiN2QyMTg4NzIxMWIxYjFjMTRjOTdhYjg4NGViNjQ2MTlhZWRjY2RiIn0.eyJhdWQiOiIzIiwianRpIjoiN2JiZmU1MjhiMjk5ZWE4ZmM1ODg5Yzc3Yzk1ZmMwOWI4ZWY2YjQ0Y2I3ZDIxODg3MjExYjFiMWMxNGM5N2FiODg0ZWI2NDYxOWFlZGNjZGIiLCJpYXQiOjE1NTEwMTEwMDcsIm5iZiI6MTU1MTAxMTAwNywiZXhwIjoxNTgyNTQ3MDA3LCJzdWIiOiIxOTMiLCJzY29wZXMiOltdfQ.enC6mmwbQPNgociOwaJbuWeGIL0v5t_54_cFyq-4UF-GkL6qiPHN0iTzwoVn3dT8SYO3zQoHF9ZiDNZt1HsdMowPp0qDS48OYC1yOk4jjMJpm6bG3a14dBT5C_lbutJxR1Hy64KZSM0AZDBQVI6wSBVsUX9PB5NO96bkaOfoojIFln5hJoszTe38ipurz0aFKfV-EN1lRT4WH603q7vAuepPWDy9-XrTEh13MrYBte6ioP0Jfspeoe35Kfv-0S0965tBTCzOzSHjEnPDAP8f3pjgqcrVk_RnEzibcI50A-DiZxjtuRJhzS8_KFfSoBiicbBaW-aTsU0VX2RM2ZLZ4sfgCsW_hm_8DLIfZBDySjS29uwjiRL1Z8gXYgr4fKw9xqGAKvTPcAqeDpDxgyeaS5tJI0R-x_PLl40dIWZPomymtF75QEOv_OwgLLvuFdY7Hu7lfe8q3RKYcr5g4dt4HYlJq1h4F1UyRowAcZXLTk5lCHzv1-KzKbeZ5ZfpkxzyIOQsP5xVDZDo0RMQ2aC7Jt000k8jkpH5DtkAR40G_mh7ffcNgpIQAFYIxXC7dAn6FVPiIv1o-xweRQa1P1j9AffpEABo6eYrrGFIihok5qOkLm4dSwUAnxB-Kfa0doz-7JJmzDTRH6zCsyvYl2oysHuXTesn1rieFFDThDErq3Q";
 
+    TextView tvName, tvDesignation, tvLocation, tvQualificationValue, tvWorkExperienceValue, tvExpectedCtcValue;
+
+    TextView tvCurrentlyWorkingValue, tvDesignationValue, tvTabFieldValue;
+
+    ImageView ivProfilePic, ivMapIcon;
+
+    Button btnConfirm, btnEditProfile;
+
+    Employee employee = new Employee();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
+        findViewsById();
+
+        //ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
 
         //Call<Employee> call = apiService.getEmployeeDetails(74, authorization);
 
-        Call<Employee> call = apiService.getEmployeeDetails(74);
+        //Call<Employee> call = apiService.getEmployeeDetails(74);
+
+        Call<Employee> call = ApiClient
+                .getInstance()
+                .getApi()
+                .getEmployeeDetails(74);
+
         call.enqueue(new Callback<Employee>() {
             @Override
             public void onResponse(Call<Employee> call, Response<Employee> response) {
-                Log.d(TAG, "onResponse: "+response.toString());
-                String empName = response.body().getName();
+
+                employee = response.body();
+
+                Log.d(TAG, "employee designation: "+ employee.getDesignation());
+
+                Log.d(TAG, "onResponse: " + response.toString());
+
+                /*String empName = response.body().getName();
                 Log.d(TAG, " Employee Name: " + empName);
                 List<Skill> skills = response.body().getSkills();
                 Log.d(TAG, "Skills are: " + skills);
+
+                employee.setName(response.body().getName());
+                employee.setDesignation(response.body().getDesignation());
+                employee.setImageUrl(response.body().getImageUrl());
+                employee.setExpectedCtc(response.body().getExpectedCtc());
+                employee.setExperience(response.body().getExperience());
+                employee.setLocation(response.body().getLocation());
+                employee.setHighestQualification(response.body().getHighestQualification());
+                employee.setExperience(response.body().getExperience());
+                employee.setExpectedCtc(response.body().getExpectedCtc());
+                employee.setSkills(response.body().getSkills());
+                employee.setWorkFunctions(response.body().getWorkFunctions());
+                employee.setIndustries(response.body().getIndustries());
+                employee.setLatitude(response.body().getLatitude());
+                employee.setLongitude(response.body().getLongitude());
+
+                Log.d(TAG, "Employee Name: " + employee.getName());*/
+
             }
 
             @Override
@@ -59,10 +112,39 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Use of Glide to fetch an image from a URL and set it into specific placeholder
+        Glide.with(this)
+                .load("https://storage.googleapis.com/icif-cs/hunt/user/image20190116063258.jpeg")
+                .apply(RequestOptions.circleCropTransform())
+                .into(ivProfilePic);
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You confirmed it.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Want to edit the profile?", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ivMapIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 19.182755, 72.84016);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -70,9 +152,26 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void findViewsById() {
+        tvName = findViewById(R.id.tvName);
+        tvDesignation = findViewById(R.id.tvDesignation);
+        tvLocation = findViewById(R.id.tvLocation);
+        tvQualificationValue = findViewById(R.id.tvQualificationValue);
+        tvWorkExperienceValue = findViewById(R.id.tvWorkExperienceValue);
+        tvExpectedCtcValue = findViewById(R.id.tvExpectedCtcValue);
+        tvCurrentlyWorkingValue = findViewById(R.id.tvCurrentlyWorkingValue);
+        tvTabFieldValue = findViewById(R.id.tvTabFieldValue);
+        tvDesignationValue = findViewById(R.id.tvDesignationValue);
+        ivProfilePic = findViewById(R.id.ivProfilePic);
+        ivMapIcon = findViewById(R.id.ivMapIcon);
+        btnConfirm = findViewById(R.id.btnConfirm);
+        btnEditProfile = findViewById(R.id.btnEditProfile);
+
+    }
+
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -91,12 +190,22 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_maps) {
-            return true;
+        switch (id) {
+            case R.id.action_maps:
+                String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 19.182755, 72.84016);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                this.startActivity(intent);
+                return true;
+
+            case R.id.action_chat:
+                Toast.makeText(this, "You clicked on the chat icon", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+
         }
 
-        return super.onOptionsItemSelected(item);
     }
 
 }
